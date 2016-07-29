b68703c802de35e3f7d668905a83ff8d7bdb4c54
GASSAG16 <alexeygrishanin@mail.ru>
2016-07-29 17:55:31 +0400

    Классы

---
+++ Shake/Shake/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shake
+{
+    class Point
+    {
+        public int x, y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
