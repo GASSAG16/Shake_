6d891b516c0448bed971258f3b4396fbe86d709c
GASSAG16 <alexeygrishanin@mail.ru>
2016-07-29 17:42:22 +0400

    Классы

--- Shake/Shake/Program.cs
+++ Shake/Shake/Program.cs
@@ -9,24 +9,21 @@ namespace Shake
     {
         static void Main(string[] args)
         {
-            int x1 = 1,y1=3;
-            char sym = '*';
+            Point p1 = new Point();
+            p1.x=1;
+            p1.y=3;
+            p1.sym='*';
 
-            Draw(x1, y1, sym);
+            p1.Draw();
             
-            int x2 = 4, y2 = 5;
-            char sym2 = '#';
-
-            Draw(x2, y2, sym2);
-
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            
+            p2.Draw();
+            
             Console.ReadLine();
-        }
-
-
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
         }
     }
 }
