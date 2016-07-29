0efb96ac8164f90c25bcee4ad416284e09f4ba5a
GASSAG16 <alexeygrishanin@mail.ru>
2016-07-29 18:41:52 +0400

    Список

--- Shake/Shake/Program.cs
+++ Shake/Shake/Program.cs
@@ -9,19 +9,41 @@ namespace Shake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x =1;
-            p1.y=3;
-            p1.sym='*';
+            Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
-
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
+            Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
+           List <char> charlist = new List<char>();
+            charlist.Add('*');
+            charlist.Add('#');
+            
+            
+            List<int> numlist = new List<int>();
+            numlist.Add(0);
+            numlist.Add(1);
+            numlist.Add(2);
+
+            int x = numlist[0];
+            int y = numlist[1];
+            int z = numlist[2];
+            
+            char rt = charlist[0];
+
+
+            foreach (var  i in numlist)
+            {
+                Console.WriteLine(i);
+            }
+            numlist.RemoveAt(0);
+
+            List<Point> plist =new List<Point>();
+            plist.Add(p1);
+            plist.Add(p2);
+
+
+           
             Console.ReadLine();
         }
     }
