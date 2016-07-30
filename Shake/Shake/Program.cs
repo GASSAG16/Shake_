f29ebdbb3aa8e0cddda86bfbbb2e0f974c040f4f
GASSAG16 <alexeygrishanin@mail.ru>
2016-07-31 03:21:42 +0400

    Управляемая "Змейка"

--- Shake/Shake/Program.cs
+++ Shake/Shake/Program.cs
@@ -10,7 +10,10 @@ namespace Shake
     {
         static void Main(string[] args)
         {
-
+            Console.SetBufferSize(80, 25);
+            
+            
+            
             HorizontLine line = new HorizontLine(0, 78, 0, '+');
             HorizontLine line2 = new HorizontLine(0, 78, 24, '+');
             VerticalLine line3 = new VerticalLine(0, 24, 0, '+');
@@ -25,25 +28,17 @@ namespace Shake
             
             Shake shake = new Shake(p,4,Direction.RIGHT);
             shake.drow();
-            shake.Move();
-            Thread.Sleep(300);
-            shake.Move();
-            Thread.Sleep(300);
-            shake.Move();
-            Thread.Sleep(300);
-            shake.Move();
-            Thread.Sleep(300);
-            shake.Move();
-            Thread.Sleep(300);
-            shake.Move();
-            Thread.Sleep(300);
-            shake.Move();
-            Thread.Sleep(300);
-            shake.Move();
-            Thread.Sleep(300);
-            shake.Move();
 
-            Console.ReadLine();
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    shake.HandleKey(key.Key);
+                }
+                Thread.Sleep(100);
+                shake.Move();
+            }
         }
     }
 }
